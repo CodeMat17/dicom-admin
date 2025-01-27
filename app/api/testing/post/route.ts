be5601814a919dc0d9d50bed831374bf7c7c357c
CodeMat17 <codemat.biz@gmail.com>
2025-01-27 01:56:23 +0100

    updated

--- app/api/testing/post/route.ts
+++ app/api/testing/post/route.ts
@@ -1,34 +1,34 @@
-// /pages/api/achievements/post.ts
+// // /pages/api/achievements/post.ts
 
-import { NextApiRequest, NextApiResponse } from "next";
-import { supabaseServiceClient } from "@/utils/supabase/service";
+// import { NextApiRequest, NextApiResponse } from "next";
+// import { supabaseServiceClient } from "@/utils/supabase/service";
 
 
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  // Ensure the method is POST
-  if (req.method === "POST") {
-    const { title, content } = req.body;
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   // Ensure the method is POST
+//   if (req.method === "POST") {
+//     const { title, content } = req.body;
 
-    try {
-      const { data, error } = await supabaseServiceClient
-        .from("testing")
-        .insert([{ title, content }]).select();
+//     try {
+//       const { data, error } = await supabaseServiceClient
+//         .from("testing")
+//         .insert([{ title, content }]).select();
 
-      if (error) throw error;
+//       if (error) throw error;
 
-      res
-        .status(200)
-        .json({ message: "Testing posted successfully", data });
-    } catch (error) {
-      res.status(500).json({ error: error });
-    }
-  } else {
-    // Return 405 if the method is not POST
-    res.setHeader("Allow", ["POST"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
-}
+//       res
+//         .status(200)
+//         .json({ message: "Testing posted successfully", data });
+//     } catch (error) {
+//       res.status(500).json({ error: error });
+//     }
+//   } else {
+//     // Return 405 if the method is not POST
+//     res.setHeader("Allow", ["POST"]);
+//     res.status(405).end(`Method ${req.method} Not Allowed`);
+//   }
+// }
